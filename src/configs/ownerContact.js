a8cd1e13765677075c667e5916fcc9cafdb8effd
Babuperumana <babuperumana@gmail.com>
2024-03-09 22:38:31 +0530

    Update ownerContact.js

--- src/configs/ownerContact.js
+++ src/configs/ownerContact.js
@@ -1,8 +1,8 @@
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Meu Dono\n' // full name
-            + 'ORG:Anny Bot;\n' // the organization of the contact
-            + 'TEL;type=CELL;type=VOICE;waid=559887583208:+55 98 8758-3208\n' // WhatsApp ID + phone number
+            + 'FN:Babu Perumana\n' // full name
+            + 'ORG:Kaipully Temple;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=918907959595:+91 89 0795-9595\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 
 module.exports = vcard
